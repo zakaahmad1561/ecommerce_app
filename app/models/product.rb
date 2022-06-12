f3963291b9aa24b58ec773f27981d2cc43a363e8
Zaka Ahmad <zackparacha@gmail.com>
2022-06-13 02:50:26 +0500

    add font

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@ class Product < ApplicationRecord
     belongs_to :user
     has_many :pictures, as: :imageable
     accepts_nested_attributes_for :pictures
+    def image_as_thumbnail(p)
+        p.variant(resize_to_limit: [900,900]).processed
+    end
 end
