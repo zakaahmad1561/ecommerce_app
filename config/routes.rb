079a78ecd3145ca0f54157b2831876bf714d3ef0
Zaka Ahmad <zackparacha@gmail.com>
2022-06-19 17:16:57 +0500

    updated as per requirements

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "pages#estore"
+  get "pages/account"
+  get "pages/blogs"
+  get "pages/cart"
+  get "pages/comment"
+  get "pages/contact"
+  get "pages/estore"
+  get "pages/men"
+  get "pages/registrations"
 end
